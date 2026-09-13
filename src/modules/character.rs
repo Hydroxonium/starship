51799059a563aaaa8d38cdbd49aa027764779e04
Hydroxonium <jawachi03@gmail.com>
2026-09-13 13:47:09 +0100

    made hxcmd mode change the prompt character

--- src/modules/character.rs
+++ src/modules/character.rs
@@ -36,7 +36,7 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
     // The result: in non-vi fish, keymap is always reported as "insert"
     let mode = match (&context.shell, keymap) {
         (Shell::Fish, "default")
-        | (Shell::Zsh, "vicmd")
+        | (Shell::Zsh, "vicmd" | "hxcmd")
         | (Shell::Cmd | Shell::PowerShell | Shell::Pwsh, "vi") => ShellEditMode::Normal,
         (Shell::Fish, "visual") => ShellEditMode::Visual,
         (Shell::Fish, "replace") => ShellEditMode::Replace,
